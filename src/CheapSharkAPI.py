e3481c8e63f182f8c544855f192fa5c9e9f64849
Neil Patrick Villanueva <neilpatz16@gmail.com>
2023-09-09 22:03:07 +0800

    SteamBud day 1 commit

---
+++ src/CheapSharkAPI.py
@@ -0,0 +1,210 @@
+import requests
+import stores
+import re
+from urllib.parse import unquote
+
+
+class SearchAPI():
+
+    def __init__(self):
+        self.url_endpoint = "https://www.cheapshark.com/api/1.0/"
+        self.headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+        'content-type': "application/json",
+		'connection': "keep-alive"
+    }
+
+    def __cheapDealSearchID(self,deal_id) -> tuple:
+        req_get_store_id = requests.get(f"{self.url_endpoint}deals", 
+                                        headers=self.headers,
+                                        params ={
+                                            'id': unquote(deal_id)
+                                        }
+                                        )
+        json_respons = req_get_store_id.json()
+        store_id = json_respons['gameInfo']['storeID']
+
+        deal_link = f"https://cheapshark.com/redirect?dealID={deal_id}&k=1"
+
+        return store_id, deal_link
+    
+    def __steam(self, steamappid) -> list:
+
+        req = requests.get(f"{self.url_endpoint}games", 
+                           headers=self.headers,
+                           params={
+                               'steamAppID': steamappid
+                           }
+                           )
+
+        info_response = []
+
+        for g in req.json():
+            game_title = g['external']
+            cheapest_price = g['cheapest']
+            cheapest_deal_id = g['cheapestDealID']
+            game_thumbnail = g['thumb']
+
+            #endpoint for taking info about cheapest deal
+            deal_search = self.__cheapDealSearchID(cheapest_deal_id)
+            store_id = deal_search[0]
+            deal_redirect_link = deal_search[1]
+
+            game_data = {
+                'game_title': game_title,
+                'cheapset_price': cheapest_price,
+                'cheapest_store': stores.store_list[int(store_id)-1]['storeName'],
+                'thumbnail': game_thumbnail,
+                'deal_link': deal_redirect_link
+                        }
+            info_response.append(game_data)
+
+        return info_response
+
+    
+    def generalSearch(self, game_name, amount=0) -> list:
+        
+        game = game_name.replace(" ", "%20")
+        req = requests.get(f"{self.url_endpoint}games", 
+                           headers=self.headers,
+                           params={
+                               'title': unquote(game)
+                           }
+                           )
+
+        info_response = []
+        count = 0
+
+        for g in req.json():
+            if amount != 0 and count >= amount:
+                break
+            elif amount == 0 and count >= 10:
+                break
+            game_id = g['gameID']
+            game_title = g['external']
+            cheapest_price = g['cheapest']
+            cheapest_deal_id = g['cheapestDealID']
+            game_thumbnail = g['thumb']
+
+            deal_search = self.__cheapDealSearchID(cheapest_deal_id)
+            store_id = deal_search[0]
+            deal_redirect_link = deal_search[1]
+
+            game_data = {
+                'gameID': game_id,
+                'game_title': game_title,
+                'cheapset_price': cheapest_price,
+                'cheapest_store': stores.store_list[int(store_id)-1]['storeName'],
+                'cheapest_store_banner': f"https://www.cheapshark.com{stores.store_list[int(store_id)-1]['images']['banner']}",
+                'thumbnail': game_thumbnail,
+                'deal_link': deal_redirect_link
+                        }
+            info_response.append(game_data)
+            count+=1
+
+        return info_response
+    
+    
+    def finddeals(self, game_id, amount=0) -> dict:
+
+        req = requests.get(f"{self.url_endpoint}games", 
+                           headers=self.headers,
+                           params={
+                               'id': game_id
+                           }
+                           )
+        game_info = req.json()
+        
+        game_title = game_info['info']['title']
+        store_id = 0
+        deal_redirect_link = ""
+        game_thumbnail = game_info['info']['thumb']
+        savings = 0
+        count = 0
+
+        game_data = {'game_title': game_title, 'thumbnail': game_thumbnail, 'other_deals': []}
+        for deals in game_info['deals']:
+            if amount != 0 and count > 10:
+                break
+            if count < 10:
+                if count==0:
+                    game_data['cheapest_price'] = deals['price']
+                    deal_search = self.__cheapDealSearchID(deals['dealID'])
+                    store_id = deal_search[0]
+                    link = f"https://www.cheapshark.com{stores.store_list[int(store_id)-1]['images']['banner']}"
+                    game_data['cheapest_store_banner'] =  link
+                    game_data['cheapest_store'] = stores.store_list[int(store_id)-1]['storeName']
+                    game_data['deal_link'] = deal_search[1]
+                    savings = deals['savings']
+                    game_data['savings'] = int(float(savings))
+                    
+                else:
+                    deal_search = self.__cheapDealSearchID(deals['dealID'])
+                    store_id = deal_search[0]
+                    deal_redirect_link = deal_search[1]
+                    deal_saving =  deals['savings']
+                    link_1 = f"https://www.cheapshark.com{stores.store_list[int(store_id)-1]['images']['logo']}"
+                    game_data['other_deals'].append(
+                        {
+                            'store': stores.store_list[int(store_id)-1]['storeName'],
+                            'price': deals['price'],
+                            'deal_link': deal_redirect_link,
+                            'store_thumb': link_1,
+                            'savings': int(float(deal_saving))
+                        }
+                    )
+            count+=1
+
+        return game_data
+    
+    def dealsLookUp(self, storeID, lowerPrice=None, upperPrice=None) -> dict:
+        deal_loop_up_req = requests.get(
+            url=f"{self.url_endpoint}deals",
+            headers=self.headers,
+            params={
+                'storeID': storeID,
+                'lowerPrice': lowerPrice,
+                'upperPrice': upperPrice
+            }
+        )
+        top_deals = []
+        count=0
+        json_response = deal_loop_up_req.json()
+        while count <= 9:
+            deal = json_response[count]
+            game_title = deal['title']
+            sale_price = deal['salePrice']
+            savings = deal['savings']
+            thumbnail = deal['thumb']
+            dealID = deal['dealID']
+            storeID = deal['storeID']
+            deal_data = {
+                'game_title': game_title,
+                'sale_price': sale_price,
+                'savings': int(float(savings)),
+                'thumb': thumbnail,
+                'deal_link': self.__cheapDealSearchID(dealID)[1]
+            }
+            top_deals.append(deal_data)
+            count+=1
+
+        return {
+            'store_name': stores.store_list[int(storeID)-1]['storeName'],
+            'store_banner': f"https://www.cheapshark.com{stores.store_list[int(storeID)-1]['images']['banner']}",
+            'deals': top_deals
+        }
+    
+    def steamID(self, steamID) -> dict:
+        return self.__steam(steamID)
+    
+    def steamlink(self, steamlink) -> dict:
+        parse_steam_link = re.findall("\d+", steamlink)[0]
+        return self.__steam(parse_steam_link)
+
+    def available_stores(self) -> list:
+        available_stores = []
+        req = requests.get(f"{self.url_endpoint}stores", headers=self.headers)
+        for store in req.json():
+            if store['isActive'] == 1:
+                available_stores.append(store)
+        return available_stores
---
+++ src/bot.py
@@ -0,0 +1,223 @@
+import discord
+from dotenv import dotenv_values
+from discord.ext import commands
+from CheapSharkAPI import SearchAPI
+
+config = dotenv_values(".env")
+TOKEN = config['TOKEN']
+
+backendAPI = SearchAPI()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="with myself"))
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send(
+            embed=discord.Embed(
+                title="⭕ Invalid Command ⭕", color=0xFF0000,
+                description="Please Try again or Enter '!sbcmds' for more commands!"
+            )
+        )
+
+@bot.command()
+async def findDeals(ctx, game_id, amountSearch=None):
+    await ctx.send(embed=discord.Embed(title=f"🔎 Searching.... 🔎", color= 10026752))
+    amount_search = amountSearch if amountSearch is not None else 0
+    try:
+        game_list = backendAPI.finddeals(game_id=game_id, amount=int(amount_search))
+        await ctx.channel.purge(limit=1)
+        if game_list == []:
+            await ctx.send(embed=discord.Embed(title="❌ No Deals Found! ❌", color= 16711680))
+        else:
+            await ctx.send(embed=discord.Embed(title=f"🏷️ {len(game_list['other_deals']) +1} Deals Found! 🏷️", color= 10026752))
+        
+        embed=discord.Embed(title=f"[CHEAPEST] {game_list['game_title']}", color= 7461119, url=game_list['deal_link'])
+        embed.set_thumbnail(url=game_list['cheapest_store_banner'])
+        embed.set_image(url=game_list['thumbnail'])
+        embed.add_field(name="Cheapest Price", value=f"${game_list['cheapest_price']}", inline=False)
+        embed.add_field(name="Store", value=game_list['cheapest_store'], inline=False)
+        embed.add_field(name=f"Saved: {game_list['savings']}%", value="", inline=False)
+        embed.set_footer(text=f"Searched By: {ctx.author.display_name}")
+        await ctx.send(embed=embed)
+
+        for game in game_list['other_deals']:
+            embed=discord.Embed(title=game['store'], color= 7461119, url=game['deal_link'])
+            embed.set_thumbnail(url=game['store_thumb'])
+            embed.add_field(name="Price", value=f"${game['price']}", inline=False)
+            embed.add_field(name=f"Saved: {game['savings']}%", value="", inline=False)
+            await ctx.send(embed=embed)
+        await ctx.send(
+            embed=discord.Embed(
+                title=f"✅ Search Complete ✅", color= 10026752, 
+                description="")
+                )
+    except Exception as e :
+        await ctx.channel.purge(limit=1)
+        await ctx.send(
+            embed=discord.Embed(
+                title=f"🚧 An Error Occured [{str(e)}] 🚧", color= 16711680, 
+                description="Please Try again or Enter '!sbcmds' for more commands!")
+                )
+
+@bot.command()
+async def searchgame(ctx, game_name:str, amountSearch=None):
+    await ctx.send(embed=discord.Embed(title=f"🔎 Searching.... 🔎", color= 10026752))
+    game_name= game_name.lower()
+    amount_search = amountSearch if amountSearch is not None else 0
+    try:
+        game_list = backendAPI.generalSearch(game_name=game_name, amount=int(amount_search))
+        await ctx.channel.purge(limit=1)
+        if game_list == []:
+            await ctx.send(embed=discord.Embed(title="❌ No Deals Found! ❌", color= 16711680))
+        else:
+            await ctx.send(embed=discord.Embed(title=f"🎮 {len(game_list)} Games Found! 🎮", color= 10026752))
+        for game in game_list:
+            embed=discord.Embed(title=f"Game ID: {game['gameID']} - {game['game_title']}", color= 7461119, url=game['deal_link'] )
+            embed.set_image(url=game['thumbnail'])
+            embed.add_field(name="Cheapest Price", value=f"${game['cheapset_price']}", inline=False)
+            embed.add_field(name="Store", value=game['cheapest_store'], inline=False)
+            embed.set_footer(text=f"Searched By: {ctx.author.display_name}")
+            await ctx.send(embed=embed)
+        await ctx.send(
+            embed=discord.Embed(
+                title=f"✅ Search Complete ✅", color= 10026752, 
+                description="")
+                )
+    except Exception as e :
+        await ctx.channel.purge(limit=1)
+        await ctx.send(
+            embed=discord.Embed(
+                title="🚧 An Error Occured 🚧", color= 16711680, 
+                description="Please Try again or Enter '!sbcmds' for more commands!")
+                )
+
+@bot.command()
+async def steamID(ctx, steamappid):
+    await ctx.send(embed=discord.Embed(title=f"🔎 Searching.... 🔎", color= 10026752))
+    try:
+        game_list = backendAPI.steamID(steamappid)
+        await ctx.channel.purge(limit=1)
+        if  game_list == []:
+            await ctx.send(embed=discord.Embed(title="❌ No Deals Found! ❌", color= 16711680))
+        else:
+            await ctx.send(embed=discord.Embed(title=f"🏷️ {len(game_list)} Deals Found! 🏷️", color= 10026752))
+        for game in game_list:
+            embed=discord.Embed(title=game['game_title'], color= 7461119, url=game['deal_link']  )
+            embed.set_image(url=game['thumbnail'])
+            embed.add_field(name="Cheapest Price", value=f"${game['cheapset_price']}", inline=False)
+            embed.add_field(name="Store", value=game['cheapest_store'], inline=False)
+            embed.set_footer(text=f"Searched By: {ctx.author.display_name}")
+            await ctx.send(embed=embed)
+        await ctx.send(
+            embed=discord.Embed(
+                title=f"✅ Search Complete ✅", color= 10026752, 
+                description="")
+                )
+    except Exception as e :
+        await ctx.channel.purge(limit=1)
+        await ctx.send(
+            embed=discord.Embed(
+                title="🚧 An Error Occured 🚧", color= 16711680, 
+                description="Please Try again or Enter '!sbcmds' for more commands!")
+                )
+
+@bot.command()
+async def steamLINK(ctx, steamLink):
+    await ctx.send(embed=discord.Embed(title=f"🔎 Searching.... 🔎", color= 10026752))
+    try:
+        game_list = backendAPI.steamlink(steamLink)
+        await ctx.channel.purge(limit=1)
+        if  game_list == []:
+            await ctx.send(embed=discord.Embed(title="❌ No Deals Found! ❌", color= 16711680))
+        else:
+            await ctx.send(embed=discord.Embed(title=f"🏷️ {len(game_list)} Deals Found! 🏷️", color= 10026752))
+        for game in game_list:
+            embed=discord.Embed(title=game['game_title'], color= 7461119, url=game['deal_link'])
+            embed.set_image(url=game['thumbnail'])
+            embed.add_field(name="Cheapest Price", value=f"${game['cheapset_price']}", inline=False)
+            embed.add_field(name="Store", value=game['cheapest_store'], inline=False)
+            embed.set_footer(text=f"Searched By: {ctx.author.display_name}")
+            await ctx.send(embed=embed)
+        await ctx.send(
+            embed=discord.Embed(
+                title=f"✅ Search Complete ✅", color= 10026752, 
+                description="")
+                )
+    except Exception as e :
+        await ctx.channel.purge(limit=1)
+        await ctx.send(
+            embed=discord.Embed(
+                title="🚧 An Error Occured 🚧", color= 16711680, 
+                description="Please Try again or Enter '!sbcmds' for more commands!")
+                )
+        
+@bot.command()
+async def dealsLookUp(ctx, storeID, lowerPrice=None, upperPrice=None):
+    await ctx.send(embed=discord.Embed(title=f"🔎 Searching.... 🔎", color= 10026752))
+    try:
+        deals_list = backendAPI.dealsLookUp(storeID=storeID, lowerPrice=lowerPrice, upperPrice=upperPrice)
+        await ctx.channel.purge(limit=1)
+        if  deals_list == []:
+            await ctx.send(embed=discord.Embed(title="❌ No Deals Found! ❌", color= 16711680))
+        else:
+            await ctx.send(embed=discord.Embed(title=f"🏷️ {len(deals_list['deals'])} Deals Found! 🏷️", color= 10026752))
+        await ctx.send(
+            embed=discord.Embed(title=f"{deals_list['store_name']} Deals", color= 7461119).set_image(url=deals_list['store_banner'])
+        )
+        for game_deal in deals_list['deals']:
+            embed=discord.Embed(title=game_deal['game_title'], color= 7461119, url=game_deal['deal_link'])
+            embed.set_image(url=game_deal['thumb'])
+            embed.add_field(name="Cheapest Price", value=f"${game_deal['sale_price']}", inline=False)
+            embed.add_field(name=f"Saved: {game_deal['savings']}%", value="", inline=False)
+            await ctx.send(embed=embed)
+        await ctx.send(
+            embed=discord.Embed(
+                title=f"✅ Search Complete ✅", color= 10026752, 
+                description="")
+                )
+    except Exception as e:
+        await ctx.channel.purge(limit=1)
+        await ctx.send(
+            embed=discord.Embed(
+                title=f"🚧 An Error Occured 🚧", color= 16711680, 
+                description="Please Try again or Enter '!sbcmds' for more commands!")
+                )
+
+@bot.command()
+async def stores(ctx):
+    store_list = backendAPI.available_stores()
+    embed = discord.Embed(title="🖥️ Trusted Websites 🖥️", color= 16777215)
+    for store in store_list:
+        embed.add_field(name=f"[{store['storeID']}] {store['storeName']}", value="")
+    await ctx.send(embed=embed)
+        
+
+
+##################################################################################################
+
+@bot.command()
+async def sbcmds(ctx):
+    embed = discord.Embed(title="🤖 SteamBud Commands 🤖", color= 16777215)
+    embed.add_field(name="🔗 | !sbcmds", value='SteamBud Commands', inline=False)
+    embed.add_field(name="🔗 | !stores", value='Return Trusted Websites', inline=False)
+    embed.add_field(name='🔗 | !searchgame "<game name>" <amount **optional **max=10>', value='Search Games and Respective Game ID', inline=False)
+    embed.add_field(name="🔗 | !dealsLookUp <storeID> <lowerPrice **optional> <upperPrice **optional>", value='Return Top 10 Deals From Selected Store Based on Lower and Upper Price.', inline=False)
+    embed.add_field(name='🔗 | !findDeals <appID>', value='Search Deals Based on appID', inline=False)
+    embed.add_field(name='🔗 | !steamID <steamappid>', value='Search Deals Based on the SteamAppID', inline=False)
+    embed.add_field(name='🔗 | !steamLINK <steamlink>', value='Search Deals Based on the Steam Link', inline=False)
+    await ctx.send(embed=embed)
+
+def main(token):
+    bot.run(token)
+
+if __name__ == '__main__':
+    main(TOKEN)
+
